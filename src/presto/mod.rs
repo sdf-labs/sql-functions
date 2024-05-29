0e84b9837bfd0a124dc041fca7017eec34b85ea8
Vladimir Gapeyev <9939945+vgapeyev@users.noreply.github.com>
2024-05-29 03:19:10 +0000

    POC: length functions, for varchars and arrays -- an example with non-trivial renaming/disambiguation. Both functions are named 'length' in Presto, but have distinct UDFs in datafusion.

--- src/presto/mod.rs
+++ src/presto/mod.rs
@@ -7,6 +7,10 @@ pub use datafusion::functions::math::abs;
 pub use datafusion::functions::math::acos;
 pub use datafusion::functions::math::round;
 
+pub use datafusion::functions::unicode::character_length as length;
+
+pub use datafusion::functions_array::length::array_length_udf as length_array;
+
 // Export the functions out of this package, both as expr_fn as well as a list of functions
 export_functions!(
     (
@@ -17,5 +21,7 @@ export_functions!(
     ),
     (presto, abs, num, "TODO: abs doc comment"),
     (presto, acos, num, "TODO: acos doc comment"),
-    (presto, round, num, "TODO: round doc comment")
+    (presto, round, num, "TODO: round doc comment"),
+    (presto, length_array, arr, "TODO: length_array doc comment"),
+    (presto, length, str, "TODO: length(varchar) doc comment")
 );
