9c9e493bf27ceb3328c33458750d688b186e2c9f
Vladimir Gapeyev <9939945+vgapeyev@users.noreply.github.com>
2024-08-03 22:22:49 +0000

    regexp_position, first overload

--- src/trino/regexp_position_impl.rs
+++ src/trino/regexp_position_impl.rs
@@ -21,22 +21,31 @@ use datafusion::common::Result;
 use datafusion::error::DataFusionError;
 use datafusion::logical_expr::simplify::{ExprSimplifyResult, SimplifyInfo};
 use datafusion::logical_expr::{ColumnarValue, Expr, ScalarUDFImpl, Signature, Volatility};
+use regex::Regex;
 use std::any::Any;
+use std::sync::Arc;
 
-fn regexp_position_varchar_joniregexp_invoke(_args: &[ColumnarValue]) -> Result<ColumnarValue> {
-    Err(DataFusionError::NotImplemented(format!(
-        "Not implemented {}:{}",
-        file!(),
-        line!()
-    )))
+use crate::utils_regexp::map_rowfun__pat_hay_to_i64;
+
+fn regexp_position_varchar_joniregexp_invoke(args: &[ColumnarValue]) -> Result<ColumnarValue> {
+    map_rowfun__pat_hay_to_i64(&args[1], &args[0], Arc::new(regexp_position__rowfun))
+}
+
+fn regexp_position__rowfun(pat: &str) -> Result<Arc<dyn Fn(/*hay:*/ &str) -> i64>> {
+    let re = Regex::new(pat).map_err(|e| {
+        DataFusionError::Execution(format!(
+            "Regular expression for regexp_position did not compile: {e:?}"
+        ))
+    })?;
+    let rowfun = move |str: &str| match re.find(str) {
+        None => -1,
+        Some(m) => m.start() as i64 + 1,
+    };
+    Ok(Arc::new(rowfun))
 }
 
 fn regexp_position_varchar_joniregexp_return_type(_arg_types: &[DataType]) -> Result<DataType> {
-    Err(DataFusionError::NotImplemented(format!(
-        "Not implemented {}:{}",
-        file!(),
-        line!()
-    )))
+    Ok(DataType::Int64)
 }
 
 fn regexp_position_varchar_joniregexp_simplify(
